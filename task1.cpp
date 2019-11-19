c3ae8a24d5efc80ea46e2b320004a41b5797b498
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-11-19 13:39:07 +0300

    Update task1.cpp

--- task1.cpp
+++ task1.cpp
@@ -1,1 +1,59 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	int i, n, j, count = 1, sum = 0;
+
+	cout << "Введите размерность массива: ";
+	cin >> n;
+
+	int *a = new int[n];
+
+	for (i = 0; i < n; i++)
+	{
+		cout << "Введите элемент массива: ";
+		cin >> a[i];
+	}
+
+	for (i = 0; i <= n; i++)
+	{
+		while (a[i] == a[i + 1])
+		{
+			i++;
+			count++;
+		}
+	}
+
+	if (a[i - 2] != a[i - 1])
+	{
+		count++;
+	}
+
+	int *b = new int[n];
+
+	for (i = 0; i < count; i++)
+	{
+		b[i] = 0;
+	}
+
+	int *c = new int[n];
+
+	for (i = 0, j = 0; i < n; i++, j++)
+	{
+		c[j] = a[i];
+		while (a[i] == a[i + 1])
+		{
+
+		}
+
+	}
+
+
+	cout << count;
+
+	system("pause");
+	return 0;
+}
