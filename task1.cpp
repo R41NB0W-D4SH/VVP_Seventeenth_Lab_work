b81a442544c8463e6202c2448169b476a3684a00
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-11-24 18:22:54 +0300

    Update task1.cpp

--- task1.cpp
+++ task1.cpp
@@ -9,52 +9,59 @@ int main()
 	cout << "Введите размерность массива: ";
 	cin >> n;
 
-	int* a = new int[n];
+	if (n >= 0)
+	{ 
+		int* a = new int[n];
 
-	cout << endl << "Введите элементы массива" << endl << endl;
-	cout << "a[0] = ";
-	cin >> a[0];
-	cur_el = a[0];
+		cout << endl << "Введите элементы массива" << endl << endl;
+		cout << "a[0] = ";
+		cin >> a[0];
+		cur_el = a[0];
 
-	for (i = 1; i < n; i++)
-	{
-		cout << "a[" << i << "] = ";
-		cin >> a[i];
-		if (cur_el != a[i])
+		for (i = 1; i < n; i++)
 		{
-		count++;
+			cout << "a[" << i << "] = ";
+			cin >> a[i];
+			if (cur_el != a[i])
+			{
+			count++;
+			}
+			cur_el = a[i];
 		}
-		cur_el = a[i];
+
+		cout << endl << endl;
+
+		int* b = new int[count];
+		int* c = new int[count];
+
+		cur_el = element = a[0];
+		for (i = 1; i < n; i++)
+		{
+			if (cur_el != a[i])
+			{
+				b[j] = length;
+				c[j] = element;
+				j++;
+				length = 1;
+				element = a[i];
+			}
+			else
+			{
+				length++;
+			}
+			cur_el = a[i];
+		}
+		b[count - 1] = length;
+		c[count - 1] = element;
+
+		for (i = 0; i < count; i++)
+		{
+			cout << "b[" << i << "] = " << b[i] << "        c[" << i << "] = " << c[i] << endl << endl;
+		}
 	}
-
-	cout << endl << endl;
-
-	int* b = new int[count];
-	int* c = new int[count];
-
-	cur_el = element = a[0];
-	for (i = 1; i < n; i++)
+	else
 	{
-		if (cur_el != a[i])
-		{
-			b[j] = length;
-			c[j] = element;
-			j++;
-			length = 1;
-			element = a[i];
-		}
-		else
-		{
-			length++;
-		}
-		cur_el = a[i];
-	}
-	b[count - 1] = length;
-	c[count - 1] = element;
-
-	for (i = 0; i < count; i++)
-	{
-		cout << "b[" << i << "] = " << b[i] << "        c[" << i << "] = " << c[i] << endl << endl;
+		cout << endl <<"Размерность массива может быть только положительной. Введите размерность еще раз, следуя условиям." << endl << endl;
 	}
 
 	system("pause");
