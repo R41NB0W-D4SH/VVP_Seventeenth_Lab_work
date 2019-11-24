94c7a0010e8e25f4562e29f8d3f0051aeefeaaab
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-11-24 18:20:06 +0300

    Update task1.cpp

--- task1.cpp
+++ task1.cpp
@@ -5,54 +5,57 @@ using namespace std;
 int main()
 {
 	setlocale(0, "");
-	int i, n, j, count = 1, sum = 0;
-
+	int i, j = 0, n, count = 1, cur_el, length = 1, element;
 	cout << "Введите размерность массива: ";
 	cin >> n;
 
-	int *a = new int[n];
+	int* a = new int[n];
 
-	for (i = 0; i < n; i++)
+	cout << endl << "Введите элементы массива" << endl << endl;
+	cout << "a[0] = ";
+	cin >> a[0];
+	cur_el = a[0];
+
+	for (i = 1; i < n; i++)
 	{
-		cout << "Введите элемент массива: ";
+		cout << "a[" << i << "] = ";
 		cin >> a[i];
-	}
-
-	for (i = 0; i <= n; i++)
-	{
-		while (a[i] == a[i + 1])
+		if (cur_el != a[i])
 		{
-			i++;
-			count++;
-		}
-	}
-
-	if (a[i - 2] != a[i - 1])
-	{
 		count++;
+		}
+		cur_el = a[i];
 	}
 
-	int *b = new int[n];
+	cout << endl << endl;
+
+	int* b = new int[count];
+	int* c = new int[count];
+
+	cur_el = element = a[0];
+	for (i = 1; i < n; i++)
+	{
+		if (cur_el != a[i])
+		{
+			b[j] = length;
+			c[j] = element;
+			j++;
+			length = 1;
+			element = a[i];
+		}
+		else
+		{
+			length++;
+		}
+		cur_el = a[i];
+	}
+	b[count - 1] = length;
+	c[count - 1] = element;
 
 	for (i = 0; i < count; i++)
 	{
-		b[i] = 0;
+		cout << "b[" << i << "] = " << b[i] << "        c[" << i << "] = " << c[i] << endl << endl;
 	}
-
-	int *c = new int[n];
-
-	for (i = 0, j = 0; i < n; i++, j++)
-	{
-		c[j] = a[i];
-		while (a[i] == a[i + 1])
-		{
-
-		}
-
-	}
-
-
-	cout << count;
 
 	system("pause");
 	return 0;
