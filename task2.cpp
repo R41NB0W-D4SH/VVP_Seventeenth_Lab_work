4b91407f1d387f29a8ab33626582ff08b961c4d4
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-11-24 20:32:14 +0300

    Update task2.cpp

--- task2.cpp
+++ task2.cpp
@@ -11,29 +11,29 @@ int main()
 	cin >> l;
 
 	if (l > 0)
-	{ 
+	{
 		cout << "Введите размерность массива: ";
 		cin >> n;
 
 		if (n >= 0)
-		{ 
+		{
 			int* a = new int[n];
 			int *rewrtd = new int[n];
 			double *buff = new double[n];
 
 			cout << endl << "Введите элементы массива" << endl << endl;
-			cout << "a[0] = ";
+			cout << "a[0] : ";
 			cin >> a[0];
 			int cur_el = a[0];
 
 			for (i = 1; i < n; i++)
 			{
-				cout << "a[" << i << "] = ";
+				cout << "a[" << i << "] : ";
 				cin >> a[i];
 				if (cur_el == a[i])
 				{
-				count++;
-	//			cout << endl << "Совпадение с предыдущим элементом!" << endl;
+					count++;
+					//			cout << endl << "Совпадение с предыдущим элементом!" << endl;
 				}
 				else
 				{
@@ -43,11 +43,11 @@ int main()
 						for (j = 2; j <= count; j++)
 						{
 							buff[i - j] = 1;
-		//					cout << endl << "Буффер обновлен!" << endl;
+							//					cout << endl << "Буффер обновлен!" << endl;
 						}
 						cb += count - 1;
 					}
-		//			cout << endl << "Похожих элементов: "  << count << endl;
+					//			cout << endl << "Похожих элементов: "  << count << endl;
 					count = 1;
 				}
 				cur_el = a[i];
@@ -58,14 +58,14 @@ int main()
 				for (j = 2; j <= count; j++)
 				{
 					buff[i - j] = 1;
-		//			cout << "Буффер обновлен!" << endl;
+					//			cout << "Буффер обновлен!" << endl;
 				}
 				cb += count - 1;
 			}
 
 			for (i = 0; i < n; i++)
 			{
-		//		cout << "Буффер[" << i << "] = " << buff[i] << endl;
+				//		cout << "Буффер[" << i << "] = " << buff[i] << endl;
 			}
 
 			j = 0;
@@ -73,12 +73,12 @@ int main()
 			for (i = 0; i < n; i++)
 			{
 				if (buff[i] == 0.1)
-				{ 
+				{
 					rewrtd[j] = 0;
 					j++;
-			//		cout << endl << "Присоение нулю в элементе: " << i << endl;
+					//		cout << endl << "Присоение нулю в элементе: " << i << endl;
 				}
-			
+
 				else if (buff[i] == 1)
 				{
 					cout /*<< endl << "Серия пропущена (Длина меньше значения L)"*/ << endl;
@@ -94,18 +94,18 @@ int main()
 			cout << endl << "Перезаписанный массив" << endl << endl;
 			for (i = 0; i < n - cb; i++)
 			{
-				cout << "n: [" << i << "] = " << rewrtd[i] << endl << endl;
+				cout << "n[" << i << "] : " << rewrtd[i] << endl << endl;
 			}
 
 		}
 		else
 		{
-			cout << endl <<"Размерность массива может быть только положительной. Введите размерность еще раз, следуя условиям." << endl << endl;
+			cout << endl << "Размерность массива может быть только положительной. Введите размерность еще раз, следуя условиям." << endl << endl;
 		}
 	}
 	else
 	{
-		cout <<"L по условию больше нуля. Введите значение L, еще раз, следуя условию." << endl << endl;
+		cout << "L по условию больше нуля. Введите значение L, еще раз, следуя условию." << endl << endl;
 	}
 
 	system("pause");
